45f0cb710a853a24a3e8aa974fd6f14c0a5d8eae
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-10-28 21:05:52 +0100

    New repository created
    First file edits to creating a skeleton

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="container">
+      <nav class="d-flex justify-content-between">
+        <a href="/" title="Homepage"/>
+        <ul>
+          <li class="active"><a href="/" title="Homepage">Home</a></li>
+          <li><a href="/" title="Recipe generator">Create</a></li>
+        </ul>
+      </nav>
+    </div>
+    </div>
+  );
+}
+
+export default App;
