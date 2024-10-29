bc8e1f848ff81db3e7e1acf482a9f2d3e1e7b57d
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-10-29 21:28:07 +0100

    Cleaning up the files and code
    Deleting initial react code and files I don't need to proceed with my project.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ function App() {
   return (
     <div className="App">
      <div class="container">
-      <nav class="d-flex justify-content-between">
-        <a href="/" title="Homepage"/>
-        <ul>
-          <li class="active"><a href="/" title="Homepage">Home</a></li>
-          <li><a href="/" title="Recipe generator">Create</a></li>
-        </ul>
-      </nav>
+     
     </div>
     </div>
   );
